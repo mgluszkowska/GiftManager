5af462fddd7a507a166ba01c109e9b65ee8c4375
Marta Głuszkowska <marglu2@st.amu.edu.pl>
2019-01-19 22:03:38 +0100

    Trying modals

--- users.php
+++ users.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
-  function redir(id) {
-      
+  function function() {
+      <?php echo "Popup text" ?>
     //window.location = "gifts.php";
   }
 </script>
@@ -12,6 +12,7 @@
         <title>Użytkownicy</title>
         <meta charset="utf-8">
         <META http-equiv=content-type content="text/html; charset=iso-8859-2">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
     <body>
         <h1>Lista wszystkich użytkowników</h1><br>
@@ -20,6 +21,7 @@
             <th>Id</th>
             <th>Imię</th> 
             <th>Nazwisko</th>
+            <th></th>
             <th></th>
             <th></th>
         </tr>
@@ -33,8 +35,10 @@ foreach($data as $item){
         <td> <?php echo $item->id."" ?></td>
         <td> <?php echo $item->name."" ?></td>
         <td> <?php echo $item->surname."" ?></td>
-        <td> <button type="button"><a href="http://www.your-website.com">Click me</a></button></td>
-        
+        <td> <button type="button"><a href=<?php echo "https://giftmanagerppr.herokuapp.com/user".$item->id.".json"?>>Click me</a></button></td>
+        <td><div class="popup" onclick="function()">New
+  <span class="popuptext" id="myPopup">Popup text...</span>
+</div></td>
         
         <td><input type="button" value="Zobacz listę życzeń" onclick="redir();" /></td>
     </tr>
