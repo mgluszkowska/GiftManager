5a7f4bb1cef05b8a4d9cba761c6d97a2611f67e9
Marta Głuszkowska <marglu2@st.amu.edu.pl>
2019-01-19 17:45:24 +0100

    Trying to find header

--- posttest.php
+++ posttest.php
@@ -2,6 +2,9 @@
 
 $curl = curl_init();
 
+$data = array('name'=>'Somename',
+              'price'=>'3');
+
 curl_setopt_array($curl, array(
   CURLOPT_URL => "http://irizar.ehost.pl/martik97/newgift.php",
   CURLOPT_RETURNTRANSFER => true,
@@ -10,7 +13,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\nTestname\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+  CURLOPT_POSTFIELDS => $data,
   CURLOPT_HTTPHEADER => array(
     "Postman-Token: 1368197a-2612-4350-9ab5-05364ecef319",
     "cache-control: no-cache",
