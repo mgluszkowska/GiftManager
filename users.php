175dc68e71f90a21e4db78f9c9ea11fa70ff7ac6
Marta Głuszkowska <marglu2@st.amu.edu.pl>
2019-01-19 18:21:44 +0100

    Added users.php

---
+++ users.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Użytkownicy</title>
+        <meta charset="utf-8">
+        <META http-equiv=content-type content="text/html; charset=iso-8859-2">
+    </head>
+    <body>
+        <h1>Lista wszystkich użytkowników</h1><br>
+        <table>
+        <tr>
+            <th>Id</th>
+            <th>Imię</th> 
+            <th>Nazwisko</th>
+        </tr>
+       <?php  
+
+$json = file_get_contents('http://irizar.ehost.pl/martik97/users.json');
+$data = json_decode($json);
+foreach($data as $item){
+    ?>
+    <tr>        
+        <td> <?php echo $item->id."" ?></td>
+        <td> <?php echo $item->name."" ?></td>
+        <td> <?php echo $item->surname."" ?></td>
+    </tr>
+<?php
+} 
+?> </table>
+    </body>
+
+</html>
