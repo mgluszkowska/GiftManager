b01df7fa61497764ef7e478702eb011264709449
Marta Głuszkowska <marglu2@st.amu.edu.pl>
2019-01-19 17:37:13 +0100

    Trying to find header

--- posttest.php
+++ posttest.php
@@ -1,58 +1,30 @@
 <?php
-//$r = new HttpRequest('http://irizar.ehost.pl/martik97/newgift.php', HttpRequest::METH_POST);
-//$r->setOptions(array('cookies' => array('lang' => 'de')));
-//$r->addPostFields(array('name' => 'mike', 'price' => '8'));
-//$r->addPostFile('image', 'profile.jpg', 'image/jpeg');
-//try {
-//    echo $r->send()->getBody();
-//} catch (HttpException $ex) {
-//    echo $ex;
-//}
 
+$curl = curl_init();
 
-$url = "http://irizar.ehost.pl/martik97/newgift.php";        
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $url); 
-//curl_setopt($ch, CURLOPT_HEADER, TRUE); 
-//curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body 
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_VERBOSE, 1);
-//$head = curl_exec($ch); 
-//$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
-//curl_close($ch); 
-//var_dump($head);
-//var_dump($httpCode);
-//$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-//$header = substr($head, 0, $header_size);
-//$body = substr($head, $header_size);
-//var_dump($header_size);
-//var_dump($header);
-//echo $body;
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://irizar.ehost.pl/martik97/newgift.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\nTestname\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+  CURLOPT_HTTPHEADER => array(
+    "Postman-Token: 1368197a-2612-4350-9ab5-05364ecef319",
+    "cache-control: no-cache",
+    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+  ),
+));
 
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-$headers = [];
-//curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_close($curl);
 
-// this function is called by curl for each header received
-curl_setopt($ch, CURLOPT_HEADERFUNCTION,
-  function($curl, $header) use (&$headers)
-  {
-    $len = strlen($header);
-    $header = explode(':', $header, 2);
-    if (count($header) < 2) // ignore invalid headers
-      return $len;
-
-    $name = strtolower(trim($header[0]));
-    if (!array_key_exists($name, $headers))
-      $headers[$name] = [trim($header[1])];
-    else
-      $headers[$name][] = trim($header[1]);
-
-    return $len;
-  }
-);
-
-$data = curl_exec($ch);
-print_r($headers);
-?>
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
