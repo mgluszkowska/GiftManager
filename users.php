ac583d0b635f6269c6dcfcf09a38cf0174bad766
Marta Głuszkowska <marglu2@st.amu.edu.pl>
2019-01-19 18:36:10 +0100

    Fixing buttons in users table

--- users.php
+++ users.php
@@ -23,7 +23,7 @@ foreach($data as $item){
         <td> <?php echo $item->id."" ?></td>
         <td> <?php echo $item->name."" ?></td>
         <td> <?php echo $item->surname."" ?></td>
-        <td><button type="button">Zobacz listę życzeń</button></td>
+        <td><button type="submit" name="sub" value="call">Zobacz listę życzeń</button></td>
     </tr>
 <?php
 } 
